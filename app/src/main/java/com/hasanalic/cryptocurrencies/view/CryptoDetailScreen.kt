967a7e690286dde5e22a229aa04cefaa3a5a581e
Hasan Ali Caliskan <hasanali7295@gmail.com>
2023-05-26 00:45:40 +0300

    added NavHost and updated dependencies

---
+++ app/src/main/java/com/hasanalic/cryptocurrencies/view/CryptoDetailScreen.kt
@@ -0,0 +1,14 @@
+package com.hasanalic.cryptocurrencies.view
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun CryptoDetailScreen(
+    id: String,
+    price: String,
+    navController: NavController
+) {
+    Text(text = "CryptoDetailScreen!")
+}
---
+++ app/src/main/java/com/hasanalic/cryptocurrencies/view/CryptoListScreen.kt
@@ -0,0 +1,11 @@
+package com.hasanalic.cryptocurrencies.view
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun CryptoListScreen(
+    navController: NavController
+) {
+
+}
