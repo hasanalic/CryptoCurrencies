2b443d46d06d69d7ede65a3be0611cf36167efc0
Hasan Ali Caliskan <hasanali7295@gmail.com>
2023-05-25 20:22:03 +0300

    added Constants

---
+++ app/src/main/java/com/hasanalic/cryptocurrencies/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.hasanalic.cryptocurrencies.util
+
+object Constants {
+
+    // https://raw.githubusercontent.com/atilsamancioglu/IA32-CryptoComposeData/main/cryptolist.json
+    // https://raw.githubusercontent.com/atilsamancioglu/IA32-CryptoComposeData/main/crypto.json
+
+    const val BASE_URL = ""
+    const val API_KEY = ""
+    const val CALL_ATTRIBUTES = ""
+}
