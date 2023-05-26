0a15a2553629537cc2a63693c332fe82e184d3e0
Hasan Ali Caliskan <hasanali7295@gmail.com>
2023-05-26 15:10:33 +0300

    added injected retrofit api

--- app/src/main/java/com/hasanalic/cryptocurrencies/di/AppModule.kt
+++ app/src/main/java/com/hasanalic/cryptocurrencies/di/AppModule.kt
@@ -1,12 +1,27 @@
 package com.hasanalic.cryptocurrencies.di
 
+import com.hasanalic.cryptocurrencies.service.CryptoAPI
+import com.hasanalic.cryptocurrencies.util.Constants
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
-
+    @Singleton
+    @Provides
+    fun injectRetrofitAPI(): CryptoAPI {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(Constants.BASE_URL)
+            .build()
+            .create()
+    }
 }
