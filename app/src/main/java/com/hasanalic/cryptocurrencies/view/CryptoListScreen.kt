dc6e660c17956b86fc8d04a8fbc36b70e5bfd8b9
Hasan Ali Caliskan <hasanali7295@gmail.com>
2023-05-27 16:56:46 +0300

    updated CryptoListScreen

--- app/src/main/java/com/hasanalic/cryptocurrencies/view/CryptoListScreen.kt
+++ app/src/main/java/com/hasanalic/cryptocurrencies/view/CryptoListScreen.kt
@@ -1,32 +1,42 @@
 package com.hasanalic.cryptocurrencies.view
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.hasanalic.cryptocurrencies.model.CryptoList
+import com.hasanalic.cryptocurrencies.model.CryptoListItem
 import com.hasanalic.cryptocurrencies.viewmodel.CryptoListViewModel
+import java.time.format.TextStyle
 
 
 @Composable
@@ -54,12 +64,14 @@ fun CryptoListScreen(
                 hint = "Search ...",
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(horizontal = 16.dp)
+                    .padding(16.dp)
             ) {
-                //viewModel.searchCryptoList(it)
+                viewModel.searchCryptoList(it)
             }
+
             Spacer(modifier = Modifier.height(10.dp))
-            // list
+
+            CryptoList(navController = navController)
         }
     }
 }
@@ -90,11 +102,88 @@ fun SearchBar(
             modifier = Modifier
                 .fillMaxWidth()
                 .shadow(5.dp, CircleShape)
-                .background(Color.White, CircleShape)
+                .background(Color.Red, CircleShape)
                 .padding(horizontal = 20.dp, vertical = 12.dp)
                 .onFocusChanged {
                     isHintDisplayed = it.isFocused != true && text.isEmpty()
                 }
         )
+        if (isHintDisplayed) {
+            Text(text = hint, color = Color.LightGray, modifier = Modifier.padding(horizontal = 20.dp, vertical = 12.dp))
+        }
+    }
+}
+
+@Composable
+fun CryptoList(
+    navController: NavController,
+    viewModel: CryptoListViewModel = hiltViewModel()
+) {
+    val cryptoList by remember { viewModel.cryptoList }
+    val errorMessage by remember { viewModel.errorMessage }
+    val isLoading by remember { viewModel.isLoading }
+
+    CryptoListView(cryptos = cryptoList, navController = navController)
+    
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        if (isLoading) {
+            CircularProgressIndicator(color = MaterialTheme.colorScheme.primary)
+        }
+        if (errorMessage.isNotEmpty()) {
+            RetryView(error = errorMessage) {
+                viewModel.loadCryptos()
+            }
+        }
+    }
+}
+
+@Composable
+fun CryptoListView(cryptos: List<CryptoListItem>, navController: NavController) {
+    LazyColumn(contentPadding = PaddingValues(5.dp)) {
+        items(cryptos.size) {cryptoIndex ->
+            CryptoRow(navController = navController, crypto = cryptos[cryptoIndex])
+        }
+    }
+}
+
+@Composable
+fun CryptoRow(navController: NavController, crypto: CryptoListItem) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = MaterialTheme.colorScheme.secondary)
+            .clickable {
+                navController.navigate("crypto_detail_screen/${crypto.currency}/${crypto.price}")
+            }
+    ) {
+        Text(
+            text = crypto.currency,
+            style = MaterialTheme.typography.headlineLarge,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.padding(2.dp)
+        )
+        Text(
+            text = crypto.price,
+            style = MaterialTheme.typography.bodyLarge,
+            fontWeight = FontWeight.Light,
+            color = MaterialTheme.colorScheme.onPrimary,
+            modifier = Modifier.padding(2.dp)
+        )
+    }
+}
+
+@Composable
+fun RetryView(error: String, onRetry: () -> Unit) {
+    Column {
+        Text(text = error, color = Color.Red, fontSize = 20.sp)
+        Spacer(modifier = Modifier.height(10.dp))
+        Button(
+            onClick = { onRetry },
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+        ) {
+            Text(text = "Retry")
+        }
     }
 }
