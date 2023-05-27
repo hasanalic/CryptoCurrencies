00d3b593db79becbe4bf37d6c57d314295defa0e
Hasan Ali Caliskan <hasanali7295@gmail.com>
2023-05-27 18:21:13 +0300

    updated CryptoDetailScreen (wrong way)

--- app/src/main/java/com/hasanalic/cryptocurrencies/view/CryptoDetailScreen.kt
+++ app/src/main/java/com/hasanalic/cryptocurrencies/view/CryptoDetailScreen.kt
@@ -1,14 +1,99 @@
 package com.hasanalic.cryptocurrencies.view
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
+import com.hasanalic.cryptocurrencies.model.Crypto
+import com.hasanalic.cryptocurrencies.util.Resource
+import com.hasanalic.cryptocurrencies.viewmodel.CryptoDetailViewModel
+import kotlinx.coroutines.launch
 
+@SuppressLint("CoroutineCreationDuringComposition")
 @Composable
 fun CryptoDetailScreen(
     id: String,
     price: String,
-    navController: NavController
+    navController: NavController,
+    viewModel: CryptoDetailViewModel = hiltViewModel()
 ) {
-    Text(text = "CryptoDetailScreen!")
+
+    // Step 1 -> Wrong
+    val scope = rememberCoroutineScope()
+    var cryptoItem by remember {
+        mutableStateOf<Resource<Crypto>>(Resource.Loading())
+    }
+    scope.launch {
+        cryptoItem = viewModel.getCrypto()
+        println(cryptoItem.data)
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.primary),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            when(cryptoItem) {
+                is Resource.Success -> {
+                    val selectedCrypto = cryptoItem.data!![0]
+                    Text(
+                        text = selectedCrypto.name,
+                        style = MaterialTheme.typography.headlineLarge,
+                        modifier = Modifier.padding(2.dp),
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.primary,
+                        textAlign = TextAlign.Center
+                    )
+                    Image(
+                        painter = rememberImagePainter(data = selectedCrypto.logo_url),
+                        contentDescription = selectedCrypto.name,
+                        modifier = Modifier
+                            .size(200.dp, 200.dp)
+                            .clip(CircleShape)
+                            .border(2.dp, Color.Gray, CircleShape)
+                    )
+                    Text(
+                        text = price,
+                        style = MaterialTheme.typography.bodyLarge,
+                        modifier = Modifier.padding(2.dp),
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.primaryContainer,
+                        textAlign = TextAlign.Center
+                    )
+                }
+                is Resource.Error -> {
+                    Text(text = cryptoItem.message!!)
+                }
+                is Resource.Loading -> {
+                    CircularProgressIndicator(color = MaterialTheme.colorScheme.onSurface)
+                }
+            }
+        }
+    }
+
 }
